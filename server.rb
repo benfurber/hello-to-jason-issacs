19df7eb51f06e266177d93c9654ee7ead2d8f070
benfurber <ben@furber-consultancy.com>
2017-08-07 08:02:42 +0100

    Correct launch command added

--- server.rb
+++ server.rb
@@ -4,7 +4,7 @@ require 'alexa_skills_ruby'
 class CustomHandler < AlexaSkillsRuby::Handler
 
   on_launch do
-    slots = request.intent.slots
+    slots = request.slots
     response.set_output_speech_text("How do you get Wittertainment on Alexa? You just get Wittertainment on Alexa.")
     #response.set_reprompt_speech_ssml("<speak>Reprompt Horoscope Text</speak>")
     response.set_simple_card("How do you get Wittertainment on Alexa?", "You just get Wittertainment on Alexa.")
